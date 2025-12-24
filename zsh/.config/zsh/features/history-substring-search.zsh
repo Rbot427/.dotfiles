c85bca5c3fbb4d4c52ce08fde4717dabcd5d146e
Ryan Stentz <rbot427@gmail.com>
2025-12-24 00:26:35 -0500

    add plugin configuration

--- zsh/.config/zsh/features/history-substring-search.zsh
+++ zsh/.config/zsh/features/history-substring-search.zsh
@@ -1,3 +1,11 @@
 PLUGIN_DIR="$ZDOTDIR/plugins/history-substring-search"
 
 source "$PLUGIN_DIR/zsh-history-substring-search.zsh"
+
+# bind UP and DOWN arrow keys to history substring search
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
--- zsh/.config/zsh/features/syntax-highlighting.zsh
+++ zsh/.config/zsh/features/syntax-highlighting.zsh
@@ -1,3 +1,10 @@
 PLUGIN_DIR="$ZDOTDIR/plugins/syntax-highlighting"
 
 source "$PLUGIN_DIR/zsh-syntax-highlighting.zsh"
+
+# Customize syntax highlights coloring
+typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=white'
+ZSH_HIGHLIGHT_STYLES[command]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=blue'
