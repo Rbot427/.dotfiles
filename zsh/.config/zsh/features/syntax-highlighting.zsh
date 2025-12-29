455ec2699e212ffbc5c2f697fc01d9dfa6cf75a0
Ryan Stentz <rbot427@gmail.com>
2025-12-29 11:36:36 -0500

    tweak zsh syntax highlighting colors

--- zsh/.config/zsh/features/syntax-highlighting.zsh
+++ zsh/.config/zsh/features/syntax-highlighting.zsh
@@ -4,7 +4,7 @@ source "$PLUGIN_DIR/zsh-syntax-highlighting.zsh"
 
 # Customize syntax highlights coloring
 typeset -A ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=white'
-ZSH_HIGHLIGHT_STYLES[command]='fg=blue'
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue'
-ZSH_HIGHLIGHT_STYLES[alias]='fg=blue'
+# ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=white'
+ZSH_HIGHLIGHT_STYLES[command]='fg=#a6e3a1'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=#a6e3a1'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=#a6e3a1'
