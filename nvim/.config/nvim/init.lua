8e0ff51f422ccd937062e98c60edf9f248f079f5
Ryan Stentz <rbot427@gmail.com>
2026-01-18 18:57:10 -0500

    document local overrides

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
+pcall(require, 'local.prehook')
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -826,6 +827,8 @@ require('lazy').setup({
     },
   },
 })
+
+pcall(require, 'local.posthook')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
