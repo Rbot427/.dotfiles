a4752541e1ea27fa9f8f255fbc1654af203077c7
Ryan Stentz <rbot427@proton.me>
2026-06-21 10:29:28 -0400

    add zsh-patina if installed

--- zsh/.config/zsh/features/syntax-highlighting.zsh
+++ zsh/.config/zsh/features/syntax-highlighting.zsh
@@ -1,10 +1,16 @@
-PLUGIN_DIR="$ZDOTDIR/plugins/syntax-highlighting"
+# Use zsh-patina if installed
+if command -v zsh-patina >/dev/null 2>&1; then
+  eval "$(zsh-patina activate)"
+else
+  PLUGIN_DIR="$ZDOTDIR/plugins/syntax-highlighting"
 
-source "$PLUGIN_DIR/zsh-syntax-highlighting.zsh"
+  source "$PLUGIN_DIR/zsh-syntax-highlighting.zsh"
 
-# Customize syntax highlights coloring
-typeset -A ZSH_HIGHLIGHT_STYLES
-# ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=white'
-ZSH_HIGHLIGHT_STYLES[command]='fg=#a6e3a1'
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=#a6e3a1'
-ZSH_HIGHLIGHT_STYLES[alias]='fg=#a6e3a1'
+  # Customize syntax highlights coloring
+  typeset -A ZSH_HIGHLIGHT_STYLES
+  # ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=white'
+  ZSH_HIGHLIGHT_STYLES[command]='fg=#a6e3a1'
+  ZSH_HIGHLIGHT_STYLES[builtin]='fg=#a6e3a1'
+  ZSH_HIGHLIGHT_STYLES[alias]='fg=#a6e3a1'
+fi
+
